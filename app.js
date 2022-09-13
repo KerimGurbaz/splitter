d622a3dc8c0180e74ab788de8c7b92b30fee93fe
KerimGurbaz <kerim.grbz.01@gmail.com>
2022-09-13 12:01:26 +0200

    js started

---
+++ app.js
@@ -0,0 +1,11 @@
+let bill = document.querySelector(".inputs-section__bill-input");
+let billNumber = parsInt(bill.value);
+
+let people = document.querySelector(".inputs-selection__people-input");
+let peopleNumber = parseInt(people.value);
+
+let tipResult = document.querySelector(".results__tip-value");
+
+let totalResult = document.querySelector(".results_total-value");
+
+let butons = document.querySelectorAll(".btns__button")
