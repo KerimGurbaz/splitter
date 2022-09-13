c04aec1d8ff9fdba1693c465cbeb1f14b116d947
KerimGurbaz <kerim.grbz.01@gmail.com>
2022-09-13 12:20:53 +0200

    updated

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let bill = document.querySelector(".inputs-section__bill-input");
-let billNumber = parsInt(bill.value);
+let billNumber = parseInt(bill.value);
 
 let people = document.querySelector(".inputs-selection__people-input");
 let peopleNumber = parseInt(people.value);
@@ -8,4 +8,22 @@ let tipResult = document.querySelector(".results__tip-value");
 
 let totalResult = document.querySelector(".results_total-value");
 
-let butons = document.querySelectorAll(".btns__button")
+let butons = document.querySelectorAll(".btns__button");
+
+let alert = document.querySelector("#alert");
+
+let tipValue = 5;
+
+console.log(butons);
+
+calculateTip();
+
+butons.forEach((element) =>{
+    element.addEventListener("click", (event) =>{
+        removeActive();
+        element.classList.add("btns__button--selected");
+
+
+
+    })
+})
