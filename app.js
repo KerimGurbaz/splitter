7c0129c4c6f01f41c779ebfb89fee584efff8545
KerimGurbaz <kerim.grbz.01@gmail.com>
2022-09-13 18:18:38 +0200

    done

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 let bill = document.querySelector(".inputs-section__bill-input");
 let billNumber = parseInt(bill.value);
 
@@ -23,7 +24,76 @@ butons.forEach((element) =>{
         removeActive();
         element.classList.add("btns__button--selected");
 
+        tipValue = parseInt(event.target.innerText.slice(0,-1))
 
-
+        calculateTip();
     })
-})
+});
+
+function removeActive(){
+    butons.forEach((element)=>{
+        element.classList.remove("btns__button--selected")
+    })
+}
+
+
+bill.addEventListener("input", () =>{
+    billNumber = parseFloat(bill.value);
+    if(billNumber == 0 || isNaN(billNumber)){
+        tipResult.innerText = 0;
+        totalResult.innerText = 0
+    }
+    else{
+        calculateTip()
+    }
+});
+
+let custom = document.querySelector(".btns__custom");
+custom.addEventListener("click", ()=>{
+    removeActive();
+
+});
+
+custom.addEventListener("input", () =>{
+    tipValue = parseInt(custom.value);
+    if(isNaN(tipValue)){
+
+    } else{
+        calculateTip();
+    }
+});
+
+
+people.addEventListener("input", () =>{
+    peopleNumber = parseFloat(people.value);
+    if(people.value ==0 || isNaN(peopleNumber)){
+        people.getElementsByClassName.borderColor = "rgb(192,36,36)";
+        alert.classList.add("error");
+        tipResult.innerText = 0;
+        totalResult.innerText = 0;
+    } else{
+        alert.classList.remove("error");
+        people.getElementsByClassName.borderColor = "hsl(189, 41%, 97%)";
+        calculateTip()
+    }
+});
+
+
+let reset = document.querySelector(".result-section__reset");
+reset.addEventListener("click", ()=>{
+    bill.value =0;
+    billNumber = 0;
+    people.value = 5;
+    peopleNumber = 5;
+    custom.value = "Custom";
+    calculateTip();
+
+});
+
+
+
+function calculateTip(){
+    tipResult.innerText = ((billNumber * tipValue) /100 /peopleNumber).toFixed(2);
+
+    totalResult.innerText = (((billNumber * tipValue) /100 + billNumber) / peopleNumber).toFixed(2);
+}
